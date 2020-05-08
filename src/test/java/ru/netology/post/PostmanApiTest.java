628089006b896404941ffe24a0e92be0f2b63d43
vika <v.sapunkova@neurobotics.ru>
2020-05-08 11:16:25 +0300

    changed JsonPath

--- src/test/java/ru/netology/post/PostmanApiTest.java
+++ src/test/java/ru/netology/post/PostmanApiTest.java
@@ -3,22 +3,19 @@ package ru.netology.post;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 class PostmanApiTest {
     @Test
     void shouldReturnData() {
-        // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-        // Выполняемые действия
                 .when()
                 .post("/post")
-        // Проверки
                 .then()
                 .statusCode(200)
-                .body(data == "some data" != null)
+                .body("data", equalTo("some data"))
         ;
     }
 }
