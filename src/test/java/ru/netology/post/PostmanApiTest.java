7add853a182d42da88e682678193dd1b78697fa0
vika <v.sapunkova@neurobotics.ru>
2020-05-08 11:18:54 +0300

    fail

--- src/test/java/ru/netology/post/PostmanApiTest.java
+++ src/test/java/ru/netology/post/PostmanApiTest.java
@@ -15,7 +15,7 @@ class PostmanApiTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("args.data.first();", equalTo("some data"))
         ;
     }
 }
